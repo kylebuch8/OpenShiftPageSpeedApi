02c1153aad47c2c837da72711ea0978dc188716f
Kyle Buchanan <kylebuch8@gmail.com>
2015-11-20 09:41:53 -0500

    added index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const Hapi = require('hapi');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+var mongoUrl;
+
+if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+    mongoUrl = process.env.OPENSHIFT_MONGODB_DB_URL + 'pagespeed';
+} else {
+    mongoUrl = 'mongodb://localhost:27017/pagespeed';
+}
+
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            cors: true
+        }
+    }
+});
+
+var getResults = (db, callback) => {
+    var collection = db.collection('results');
+    collection.find().toArray((err, items) => {
+        assert.equal(err, null);
+
+        if (items != null) {
+            callback(items);
+        }
+    });
+};
+
+server.connection({
+    host: process.env.OPENSHIFT_NODEJS_IP || 'localhost',
+    port: process.env.OPENSHIFT_NODEJS_PORT || 8000
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+        MongoClient.connect(mongoUrl, (err, db) => {
+            assert.equal(null, err);
+            getResults(db, (items) => {
+                db.close();
+                return reply(items);
+            });
+        });
+    }
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log('Server running at: ', server.info.uri);
+});
