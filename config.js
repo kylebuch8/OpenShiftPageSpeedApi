b81282b53a6fcf76711893073d17cc683b9c050e
Kyle Buchanan <kylebuch8@gmail.com>
2015-12-04 14:40:23 -0500

    tracking more pages

--- config.js
+++ config.js
@@ -3,6 +3,26 @@ var config = {
         {
             url: 'https://access.redhat.com',
             strategies: ['desktop', 'mobile']
+        },
+        {
+            url: 'https://access.redhat.com/search',
+            strategies: ['desktop', 'mobile']
+        },
+        {
+            url: 'https://access.redhat.com/solutions/100003',
+            strategies: ['desktop', 'mobile']
+        },
+        {
+            url: 'https://access.redhat.com/articles/10009',
+            strategies: ['desktop', 'mobile']
+        },
+        {
+            url: 'http://www.redhat.com/',
+            strategies: ['desktop', 'mobile']
+        },
+        {
+            url: 'http://developers.redhat.com',
+            strategies: ['desktop', 'mobile']
         }
     ]
 };
